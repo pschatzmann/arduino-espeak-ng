e5d1340532620bd07fe160b6c58fea3d009cf82a
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-02 18:02:52 +0100

    compile errors for MBED

--- arduino/direntx.h
+++ arduino/direntx.h
@@ -1,6 +1,6 @@
 
 #pragma once
-#if defined(ARDUINO_ARCH_STM32) || defined(ARDUINO_ARCH_RP2040)
+#if defined(ARDUINO_ARCH_STM32) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_MBED)
 // make dirent available
 #  include "ConfigFS.h" // https://github.com/pschatzmann/arduino-posix-fs
 #else
